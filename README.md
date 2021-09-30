8dabc73c26824aaf344c85b4f4adb01049d11d2d
AhadCove <ahadcove@gmail.com>
2021-09-30 13:54:41 -0500

    fix: readme

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Lumia Stream API Demo
 
-## Check out the full tutorial at [lumiastream/dev](https://lumiastream.com/dev)
+## Check out the full tutorial at [dev.lumiastream.com](https://dev.lumiastream.com/docs/intro)
 
 ### Summary
-This is the demo that will trigger the Lumia Stream API
+
+This is a complimentary demo that will show the various ways to use Lumia Streams Developers API
 
 ### How to use
+
 1) Clone the repository `git clone https://github.com/lumiastream/lumiastream-api-demo-py.git`
 2) CD into the repo and Run `pip install --requirement requirements.txt`
-3) Start Lumia Stream and Turn on the Rest API by going to `Settings > Advanced > Enable REST Api
-4) After the REST API is enabled, you'll be presented with a token in Lumia Stream. Copy it
+3) Start Lumia Stream and Turn on the Developers API by going to `Settings > Advanced > Enable Developers Api`
+4) After the Developers API is enabled, you'll be presented with a token in Lumia Stream. Copy it
 5) Open up `main.py` in the demo api and paste the api token to token
 6) Run the demo api with `python3 main.py`
 
-This is the simple version of the api that will just trigger a chat color
-The REST Api for Lumia Stream can trigger a lot more than this, visit the docs above to see all it can do
+Visit the docs above to see all the deveopers api can do
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+from time import sleep
 import requests
 
 token = 'paste-token-here'
 
+# Get settings
 response = requests.get('http://localhost:39231/api/retrieve?token=' + token)
 print('response: ', response.json())
 
-requests.post('http://localhost:39231/api/send?token=' + token, data={ 'type': 'chat-color', 'value': 'blue' })
+# Send Chat Command
+requests.post('http://localhost:39231/api/send?token=' + token,
+              data={'type': 'chat-command', 'params': {'value': 'blue'}})
+sleep(1)
+
+# Send Alert
+requests.post('http://localhost:39231/api/send?token=' + token,
+              data={'type': 'alert', 'params': {'value': 'twitch-follower'}})
+sleep(1)
+
+# Send RGB Colors with Brightness
+requests.post('http://localhost:39231/api/send?token=' + token, data={type: 'rgb-color', 'params': {
+              'value': {'r': 20, 'g': 100, 'b': 10}, 'brightness': 100, 'transition': 0, 'duration': 5000}})
+sleep(1)
+
+# Send Text To Speech
+requests.post('http://localhost:39231/api/send?token=' + token,
+              data={type: 'tts', 'params': {'value': 'Wow, this tutorial is so cool'}})
+sleep(1)
+
+# Send Chatbot message
+requests.post('http://localhost:39231/api/send?token=' + token,
+              data={type: 'chatbot-message', 'params': {'value': 'Wow, this tutorial is so cool', 'platform': 'twitch'}})
+sleep(1)
