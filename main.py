60087c23a5ff6e63d96b3d72aa8f870679ff9098
AhadCove <ahadcove@gmail.com>
2021-11-12 09:32:09 -0500

    fix: python requests

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ print('response: ', response.json())
 
 # Send Chat Command
 requests.post('http://localhost:39231/api/send?token=' + token,
-              data={'type': 'chat-command', 'params': {'value': 'blue'}})
+              json={'type': 'chat-command', 'params': {'value': 'blue'}})
 sleep(1)
 
 # Send Alert
 requests.post('http://localhost:39231/api/send?token=' + token,
-              data={'type': 'alert', 'params': {'value': 'twitch-follower'}})
+              json={'type': 'alert', 'params': {'value': 'twitch-follower'}})
 sleep(1)
 
 # Send RGB Colors with Brightness
-requests.post('http://localhost:39231/api/send?token=' + token, data={type: 'rgb-color', 'params': {
+requests.post('http://localhost:39231/api/send?token=' + token, json={type: 'rgb-color', 'params': {
               'value': {'r': 20, 'g': 100, 'b': 10}, 'brightness': 100, 'transition': 0, 'duration': 5000}})
 sleep(1)
 
 # Send Text To Speech
 requests.post('http://localhost:39231/api/send?token=' + token,
-              data={type: 'tts', 'params': {'value': 'Wow, this tutorial is so cool'}})
+              json={type: 'tts', 'params': {'value': 'Wow, this tutorial is so cool'}})
 sleep(1)
 
 # Send Chatbot message
 requests.post('http://localhost:39231/api/send?token=' + token,
-              data={type: 'chatbot-message', 'params': {'value': 'Wow, this tutorial is so cool', 'platform': 'twitch'}})
+              json={type: 'chatbot-message', 'params': {'value': 'Wow, this tutorial is so cool', 'platform': 'twitch'}})
 sleep(1)
